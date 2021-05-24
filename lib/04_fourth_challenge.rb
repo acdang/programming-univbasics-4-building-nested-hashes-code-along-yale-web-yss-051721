379e38d92601d65a093cd469a484089cc618cbcf
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-24 18:23:54 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -9,6 +9,11 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
+        },
+        :Mercutio => {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
